eb32a9aab671c49751c2f3cebcea6fc16475a4bb
ZhouJianzj <2998390669@qq.com>
2021-04-26 17:07:13 +0800

    参数化测试的 使用到的是RunWith（Parameterized.class) 静态方法上的@Parameterized.parameters

---
+++ spring/TestPratice/src/main/java/com/zhouJian/AddTest.java
@@ -0,0 +1,7 @@
+package com.zhouJian;
+
+public class AddTest {
+    public int doAdd(int a ,int b){
+        return  a + b;
+    }
+}
---
+++ spring/TestPratice/src/test/java/TestAdd.java
@@ -0,0 +1,35 @@
+import com.zhouJian.AddTest;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+/**
+ * 参数化测试
+ */
+@RunWith(Parameterized.class)
+public class TestAdd {
+    private int expected;
+    private  int input;
+    private  int output;
+    public TestAdd(int expected ,int inPut ,int outPut){
+        this.expected = expected;
+        this.input = inPut;
+        this.output = outPut;
+    }
+    @Parameterized.Parameters
+    public static Collection<Object[]> date(){
+//        测试参数的赋值，第一个是expert预期的结果，后面的是参数
+        Object [][] objects = {{5,2,3},{6,2,4}};
+        return Arrays.asList(objects);
+    }
+    @Test
+    public void testAdd(){
+        AddTest addTest = new AddTest();
+//        书写预期结果，和实际的执行方法
+        Assert.assertEquals(expected,addTest.doAdd(input,output));
+    }
+}
