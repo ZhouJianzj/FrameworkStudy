3709760820fccaea54d4dc6f4e0ecbf8a3fbffe7
ZhouJianzj <2998390669@qq.com>
2021-04-27 19:36:14 +0800

    bug解决方法： 主要的问题出在了tomcat版本的问题，之前使用的最新的10.0，导致发现不了资源文件报404错误，改回之前的8.0版本就OK了 注意： el表达式的书写问题就是不能使用被占用的关键字体 例如 do等。。。。

--- SpringMVC/DemoOne/src/main/java/com/zj/doDemo.java
+++ SpringMVC/DemoOne/src/main/java/com/zj/doDemo.java
@@ -3,20 +3,15 @@ package com.zj;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+
 @Controller
 public class doDemo {
-    /**
-     * 配置需要处理请求的url
-     *
-     */
     @RequestMapping(value = "some.do")
-    public ModelAndView doMain(){
-        ModelAndView mv = new ModelAndView();
-        //设置变量 key  和  value
-        mv.addObject("do","正在执行Loading。。。。。。");
-        mv.addObject("result","执行完毕！");
-        //请求转发的url
-        mv.setViewName("/show.jsp");
-        return mv;
+    public ModelAndView doSome(){
+        System.out.println("==============================");
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.addObject("doLoading","Loading........!");
+        modelAndView.setViewName("/show.jsp");
+        return modelAndView;
     }
 }
--- SpringMVC/DemoOne/src/main/webapp/show.jsp
+++ SpringMVC/DemoOne/src/main/webapp/show.jsp
@@ -1,18 +1,16 @@
 <%--
   Created by IntelliJ IDEA.
   User: zhoujian
-  Date: 2021/4/26
-  Time: 22:31
+  Date: 2021/4/27
+  Time: 12:16
   To change this template use File | Settings | File Templates.
 --%>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <html>
 <head>
-    <title>show</title>
+    <title>Title</title>
 </head>
 <body>
-<%--使用er表达式，前面的控制器对象发来的视图，中的变量--%>
-<h3>do:${do}</h3>
-<h3>result:${result}</h3>
+<p>${doLoading}</p>
 </body>
 </html>
