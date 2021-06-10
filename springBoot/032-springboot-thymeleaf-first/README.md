92f8ea2e290c11a7118a3475127cfe9f3ed55ce0
ZhouJianzj <2998390669@qq.com>
2021-06-10 22:16:00 +0800

    使用自带的模板引擎thymeleaf

---
+++ springBoot/032-springboot-thymeleaf-first/README.md
@@ -0,0 +1,6 @@
+# 工程简介
+1、使用thymeleaf模板引擎\
+2、使用html代替jsp\
+3、jsp是需要编译的，底层还是一个servlet\
+4、thymeleaf底层是java\
+5、具体使用需要参加实例
---
+++ springBoot/032-springboot-thymeleaf-first/src/main/java/com/zj/springbootthymeleaffirst/web/DemoContrl.java
@@ -0,0 +1,14 @@
+package com.zj.springbootthymeleaffirst.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class DemoContrl {
+    @RequestMapping(value = "/test")
+    public String test(Model model){
+        model.addAttribute("data","springboot使用thymeleaf");
+        return "message";
+    }
+}
