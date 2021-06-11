db04e1fba86889ac2e6898b10eb92b8ff4144a0f
ZhouJianzj <2998390669@qq.com>
2021-06-11 16:05:06 +0800

    thymeleaf的模板基本的表达式\ 是要到thymeleaf的参数需要添加 th 1、url的时候使用@{/test(id="参数",name="参数")}

---
+++ springBoot/033-springboot-expression/src/main/java/com/zj/springbootexpression/web/Controller.java
@@ -0,0 +1,56 @@
+package com.zj.springbootexpression.web;
+
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+
+@org.springframework.stereotype.Controller
+public class Controller {
+    @RequestMapping(value = "/methodOne")
+    public String methodOne(Model model){
+        model.addAttribute("id",1001);
+        model.addAttribute("name","zhoujian");
+        return "message";
+    }
+
+
+    @RequestMapping(value = "/methodTwo")
+    public String methodTwo(Model model, HttpServletRequest request){
+        model.addAttribute( "id",request.getParameter("id"));
+        model.addAttribute( "name",request.getParameter("name"));
+        return "exhibit";
+    }
+
+    /**
+     * 使用restful风格
+     * @param model
+     * @param id
+     * @param name
+     * @return
+     */
+    @RequestMapping(value = "/restful/{id}/{name}")
+    public String restful(Model model,
+                          @PathVariable("id") Integer id,
+                          @PathVariable("name") String name){
+        model.addAttribute("id",id);
+        model.addAttribute("name",name);
+        return "restful";
+    }
+
+    /**
+     * 这里的id name什么的可以自动的注进来，也可以通过HttpServletRequest获取
+     * @param id
+     * @param name
+     * @param request
+     * @return
+     */
+    @RequestMapping(value = "/doSubmit")
+    @ResponseBody
+    public String  doSubmit(Integer id,String name,HttpServletRequest request){
+        System.out.println(request.getParameter("id"));
+        return id + ":" + name;
+    }
+}
