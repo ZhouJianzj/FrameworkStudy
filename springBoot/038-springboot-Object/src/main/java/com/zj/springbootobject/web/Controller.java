2195de21f86db048cc9b1d560bf40f9461ea74e1
ZhouJianzj <2998390669@qq.com>
2021-06-14 21:05:16 +0800

    内部对象的使用\
    #Object

---
+++ springBoot/038-springboot-Object/src/main/java/com/zj/springbootobject/web/Controller.java
@@ -0,0 +1,16 @@
+package com.zj.springbootobject.web;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+@org.springframework.stereotype.Controller
+public class Controller {
+    @RequestMapping(value = "/object")
+    public String doObject(HttpServletRequest request){
+        HttpSession session = request.getSession();
+        session.setAttribute("data","zhoujian");
+        return "Object";
+    }
+}
