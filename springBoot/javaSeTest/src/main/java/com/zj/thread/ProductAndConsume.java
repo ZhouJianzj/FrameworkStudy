f918d696de46befd54bcb5e571707d6fd1471e13
ZhouJianzj <2998390669@qq.com>
2021-07-13 23:35:50 +0800

    生产消费模式的实现，死锁的实现

---
+++ springBoot/javaSeTest/src/main/java/com/zj/thread/ProductAndConsume.java
@@ -0,0 +1,72 @@
+package com.zj.thread;
+
+import java.util.ArrayList;
+
+/**
+ * @author zhoujian
+ */
+public class ProductAndConsume {
+    public static void main(String[] args) {
+        ArrayList arrayList = new ArrayList();
+        Product product = new Product(arrayList);
+        Consumer consumer = new Consumer(arrayList);
+        product.start();
+        consumer.start();
+    }
+}
+class Product  extends Thread{
+    private ArrayList arrayList;
+    @Override
+    public void run() {
+        synchronized (arrayList){
+            while(true){
+                if (arrayList.size() <= 0){
+                    arrayList.add(new Object());
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    System.out.println(Thread.currentThread().getName() + "生产成功");
+                }
+                try {
+//                    使线程等待，释放对象锁
+                   arrayList.wait();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+                arrayList.notify();
+            }
+        }
+    }
+
+    public Product(ArrayList arrayList) {
+        this.arrayList = arrayList;
+    }
+}
+class Consumer extends Thread{
+    private ArrayList arrayList;
+    @Override
+    public void run() {
+        synchronized (arrayList){
+            while(true){
+                if (arrayList.size() > 0){
+                    arrayList.remove(0);
+                    System.out.println(Thread.currentThread().getName() + "移除成功");
+                }
+//                唤醒wait中的线程，本线程不释放锁还在执行
+                arrayList.notify();
+                try {
+                    arrayList.wait();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+    public Consumer(ArrayList arrayList) {
+        this.arrayList = arrayList;
+    }
+}
+
---
+++ springBoot/javaSeTest/src/main/java/com/zj/thread/TimeTest.java
@@ -0,0 +1,35 @@
+package com.zj.thread;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Timer;
+import java.util.TimerTask;
+
+/**
+ * @author zhoujian
+ */
+public class TimeTest {
+    public static void main(String[] args) {
+        try {
+            Timer timer = new Timer();
+
+            SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+            Date parse = simpleDateFormat.parse("2021-7-13 9:50:00");
+
+            System.out.println(parse);
+
+            timer.schedule(new TimeTaskT(),parse,1000*2);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+    }
+}
+class TimeTaskT extends TimerTask {
+
+    @Override
+    public void run() {
+        System.out.println("============时间到++++++++++++");
+    }
+}
+
