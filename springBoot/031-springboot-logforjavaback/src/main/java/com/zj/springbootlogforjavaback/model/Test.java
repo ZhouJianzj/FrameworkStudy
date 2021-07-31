37a44f7d8d8fe1cb959033a26865a65ed2f421cd
ZhouJianzj <2998390669@qq.com>
2021-07-30 23:47:40 -0700

    lombook的@Data注解

---
+++ springBoot/031-springboot-logforjavaback/src/main/java/com/zj/springbootlogforjavaback/model/Test.java
@@ -0,0 +1,19 @@
+package com.zj.springbootlogforjavaback.model;
+
+import lombok.Data;
+
+/**
+ * @author zhoujian
+ * 这个注解不提供有参构造
+ */
+@Data
+public class Test {
+    private String name;
+    private int id;
+}
+class TestT{
+    public static void main(String[] args) {
+        Test test = new Test();
+
+    }
+}
