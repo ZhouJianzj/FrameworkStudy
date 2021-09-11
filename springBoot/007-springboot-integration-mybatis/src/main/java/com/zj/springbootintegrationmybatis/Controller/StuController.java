d3a3cb9357acba6cc67c66a3a8646e5e4c8a0c9b
ZhouJianzj <2998390669@qq.com>
2021-09-11 15:53:11 +0800

    复习springboot - mybatis - restful

--- springBoot/007-springboot-integration-mybatis/src/main/java/com/zj/springbootintegrationmybatis/Controller/StuController.java
+++ springBoot/007-springboot-integration-mybatis/src/main/java/com/zj/springbootintegrationmybatis/Controller/StuController.java
@@ -4,7 +4,9 @@ import com.zj.springbootintegrationmybatis.model.Student;
 import com.zj.springbootintegrationmybatis.service.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
@@ -14,7 +16,12 @@ public class StuController {
 
     @RequestMapping(value = "/findStu")
     @ResponseBody
-    public Student findStu(){
-        return studentService.selectStu(1);
+/**
+ * 这里的@RequestParam(value = "id")也可以不写，地址栏中的"/findStu?id=1中的id也能对应到方法中的id传递过来，默认是按照名字来自动赋值的
+ * 如果要是写了就必须有id这个属性传递 过来哪怕是没有值
+  */
+    public Student findStu( Integer id){
+        System.out.println(id);
+        return studentService.selectStu(id);
     }
 }
--- springBoot/007-springboot-integration-mybatis/src/main/java/com/zj/springbootintegrationmybatis/mapper/StudentMapper.java
+++ springBoot/007-springboot-integration-mybatis/src/main/java/com/zj/springbootintegrationmybatis/mapper/StudentMapper.java
@@ -6,10 +6,25 @@ import org.apache.ibatis.annotations.Select;
 
 @Mapper
 public interface StudentMapper {
+    /**
+     * 根据主键删除一条记录
+     * @param id
+     * @return
+     */
     int deleteByPrimaryKey(Integer id);
 
+    /**
+     * 插入一个记录
+     * @param record
+     * @return
+     */
     int insert(Student record);
 
+    /**
+     * 插入一个记录，只插入有值的属性
+     * @param record
+     * @return
+     */
     int insertSelective(Student record);
 
     @Select(value = "select * from student where id = #{id}")
