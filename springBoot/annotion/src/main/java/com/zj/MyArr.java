125100d99e5e04ecc182ce7582d9fb934de3b4df
ZhouJianzj <2998390669@qq.com>
2021-09-24 15:12:01 +0800

    自定义集合老师讲解

---
+++ springBoot/annotion/src/main/java/com/zj/MyArr.java
@@ -0,0 +1,45 @@
+package com.zj;
+
+import java.util.Arrays;
+
+/**
+ * @author zhoujian
+ */
+public class MyArr<T> {
+    Object[]arr={};
+    int size;
+
+    public void add(T t){
+        if(arr.length==0){
+            arr= Arrays.copyOf(arr,10);
+        }else if(arr.length==size){//容量不够
+            int newCapacity=arr.length+(arr.length>>1);
+            arr= Arrays.copyOf(arr,newCapacity);
+        }
+        arr[size++]=t;
+    }
+
+    public T get(int index){
+        if(index>size-1){
+            throw new ArrayIndexOutOfBoundsException(index);
+        }
+        return (T) arr[index];
+    }
+
+    public int size(){
+        return size;
+    }
+
+    @Override
+    public String toString(){
+        StringBuilder sb=new StringBuilder("[");
+        for(Object o:arr){
+            if(o==null){
+                break;
+            }
+            sb.append(o.toString()+",");
+        }
+        sb.replace(sb.length()-1,sb.length(),"]");
+        return sb.toString();
+    }
+}
