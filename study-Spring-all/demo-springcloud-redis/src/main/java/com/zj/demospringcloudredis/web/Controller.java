33e809760a038ba28009b1815f18cdee8320003b
ZhouJianzj <2998390669@qq.com>
2021-10-14 21:50:00 +0800

    zuul网关的复习，微服务架构的理解

--- study-Spring-all/demo-springcloud-redis/src/main/java/com/zj/demospringcloudredis/web/Controller.java
+++ study-Spring-all/demo-springcloud-redis/src/main/java/com/zj/demospringcloudredis/web/Controller.java
@@ -1,14 +1,11 @@
 package com.zj.demospringcloudredis.web;
 
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
-import java.util.Set;
 
 
 @RestController
