a05a4399e4c23c1b2b59557f089f4d960126daf1
ZhouJianzj <2998390669@qq.com>
2022-07-06 05:09:22 +0800

    注册中心的使用

---
+++ springcloud/cloud-demo/eureka-server/src/main/java/cn/itcast/EurekaServer.java
@@ -0,0 +1,16 @@
+package cn.itcast;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author zhoujian
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaServer {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServer.class, args);
+    }
+}
