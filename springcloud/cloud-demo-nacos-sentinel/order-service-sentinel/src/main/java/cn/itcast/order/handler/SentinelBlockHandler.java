48a7e86384511d38a3824e38f60c348a13b887b3
ZhouJianzj <2998390669@qq.com>
2022-08-10 18:24:40 +0800

    sentinel自定义限流异常响应结果

---
+++ springcloud/cloud-demo-nacos-sentinel/order-service-sentinel/src/main/java/cn/itcast/order/handler/SentinelBlockHandler.java
@@ -0,0 +1,45 @@
+package cn.itcast.order.handler;
+
+import com.alibaba.csp.sentinel.adapter.spring.webmvc.callback.BlockExceptionHandler;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
+import com.alibaba.csp.sentinel.slots.block.authority.AuthorityException;
+import com.alibaba.csp.sentinel.slots.block.degrade.DegradeException;
+import com.alibaba.csp.sentinel.slots.block.flow.FlowException;
+import com.alibaba.csp.sentinel.slots.block.flow.param.ParamFlowException;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * @author zhoujian
+ *每当达到条件的时候sentinel都会去拦截并且抛出异常，但是异常响应结构都是固定的所以我们可以自定义异常响应结果
+ * 自定义异常
+ */
+@Component
+public class SentinelBlockHandler implements BlockExceptionHandler {
+    @Override
+    public void handle(HttpServletRequest request,
+                       HttpServletResponse response,
+                       BlockException e) throws Exception {
+
+        String msg = "未知异常";
+        int status = 429;
+
+        if (e instanceof FlowException) {
+            msg = "请求被限流了";
+        } else if (e instanceof ParamFlowException) {
+            msg = "请求被热点参数限流";
+        } else if (e instanceof DegradeException) {
+            msg = "请求被降级了";
+        } else if (e instanceof AuthorityException) {
+            msg = "没有权限访问";
+            status = 401;
+        }
+
+        response.setContentType("application/json;charset=utf-8");
+        response.setStatus(status);
+        response.getWriter().println("{\"msg\": " + msg + ", \"status\": " + status + "}");
+
+    }
+}
